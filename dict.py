5ffeeb456454741b483e7e4289170c28a4ca45e1
Lavanya Gumma <lavanyagumma8790@gmail.com>
2026-09-12 16:02:26 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,9 @@
+a={'name':'lavs','age':20}
+h={'name':'kavs','age':40}
+f={'name':'aavs','age':30}
+c={'name':'savs','age':50}
+b={'name':'pavs','age':21}
+for k,v in a.items():
+    print(k,v)
+for k,v in b.items():
+    print(k,v)
